5a81a9efae0645230069be4661183b5edabcfc11
NAYNKNOWS <159480324+NAYNKNOWS@users.noreply.github.com>
2024-06-14 12:58:08 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.getElementById('clickableSection').addEventListener('click', function() {
+    alert('Section clicked!');
+  });
+  document.getElementById('clickableSection2').addEventListener('click', function() {
+    alert('Section clicked!');
+  });
+  document.getElementById('clickableSection3').addEventListener('click', function() {
+    alert('Section clicked!');
+  });
+ 
